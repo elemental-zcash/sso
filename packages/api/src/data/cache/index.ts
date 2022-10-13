4dd053ed165a7a3bf16637246aeae64073f4b2b7
macintoshhelper <macintoshhelper@users.noreply.github.com>
2022-10-13 14:09:56 +0400

    deprecate cache router

--- packages/api/src/data/cache/index.ts
+++ packages/api/src/data/cache/index.ts
@@ -1,16 +1,17 @@
 /**
- * Cache router
+ * Deprecated Cache router
  */
 
-import { UserType } from '../../models';
-import { cacheGet, cacheSet } from '../../utils';
+import { OAuthTokenType, UserType } from '../../models';
+import { cacheDelete, cacheGet, cacheSet } from '../../utils';
 
 export const users = {
   getById: async (userId): Promise<UserType> => {
     const cachedUser = await cacheGet(`users:${userId}`) as UserType;
     return cachedUser;
   },
-  save: async (user) => {
+  deleteById: async () => {},
+  save: async (user): Promise<boolean> => {
     const { uuid } = user;
     if (uuid) {
       return await cacheSet(`users:${uuid}`, user);
@@ -20,15 +21,27 @@ export const users = {
 };
 
 export const tokens = {
-  getTokenByRefreshToken: async (refreshToken: string) => {
+  getTokenByRefreshToken: async (refreshToken: string): Promise<OAuthTokenType> => {
     let accessToken = await cacheGet(`accessTokenByRefreshToken:${refreshToken}`);
 
     if (accessToken) {
-      return await cacheGet(`tokens:${accessToken}`);
+      return await cacheGet(`tokens:${accessToken}`) as OAuthTokenType;
     }
     return null;
   },
-  save: async (token) => {
+  deleteByAccessToken: async (accessToken: string): Promise<boolean> => {
+    const { refreshToken} = await tokens.getByAccessToken(accessToken);
+    if (refreshToken) {
+      await cacheDelete(`accessTokenByRefreshToken:${refreshToken}`);
+    }
+    return await Boolean(cacheDelete(`tokens:${accessToken}`));
+  },
+  getByAccessToken: async (accessToken: string): Promise<OAuthTokenType> => {
+    const cachedToken = await cacheGet(`tokens:${accessToken}`) as OAuthTokenType;
+
+    return cachedToken;
+  },
+  save: async (token): Promise<boolean> => {
     const { accessToken } = token;
     if (accessToken) {
       return await cacheSet(`tokens:${accessToken}`, token);
