0044f06c4272891cf46670b47e3bdca67f0404b9
macintoshhelper <macintoshhelper@users.noreply.github.com>
2023-03-20 22:50:58 +0400

    add create clients command to app

--- packages/server/sso/app.py
+++ packages/server/sso/app.py
@@ -1,18 +1,23 @@
 # from . import db
 # from .models import Contact, ContactGroup, Gender
 
-from flask import Flask
+from flask import Flask, current_app
+from flask.cli import with_appcontext
+from sqlalchemy import select
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_login import LoginManager
 from flask_marshmallow import Marshmallow
 from apifairy import APIFairy
+import click
+import json
 
 import os
 
 from db import db
-from models import User
+from models import User, OAuth2Client
+from oauth2 import config_oauth
 
 
 # app = Flask(__name__)
@@ -23,6 +28,31 @@ apifairy = APIFairy()
 ma = Marshmallow()
 cors = CORS()
 
+# oauth2_client = OAuth2Client.query.filter_by(client_id='kG7DwrwlOC72vGX4PyZstyHx').first()
+
+# oauth2_client.set_client_metadata({
+#     "client_name": oauth2_client.metadata.client_name,
+#     "client_uri": oauth2_client.metadata.client_uri,
+#     "grant_types": oauth2_client.metadata.grant_types + ['password'],
+#     "redirect_uris": oauth2_client.metadata.redirect_uris,
+#     "response_types": oauth2_client.metadata.response_types,
+#     "scope": oauth2_client.metadata.scope,
+#     "token_endpoint_auth_method": oauth2_client.metadata.token_endpoint_auth_method,
+# })
+
+@click.command()
+@with_appcontext
+def create_clients():
+    from routes.oauth2 import generate_client
+    with open(os.path.join(current_app.root_path, "oauth_clients.json"), "r") as jsonfile:
+        data = json.load(jsonfile)
+        print("Read successful")
+    # print(data["clients"])
+    for client in data["clients"]:
+        generate_client(client)
+
+    return
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
@@ -31,6 +61,7 @@ def create_app():
     login.init_app(app)
     apifairy.init_app(app)
     ma.init_app(app)
+    config_oauth(app)
     if app.config['USE_CORS']:
         cors.init_app(app)
 
@@ -42,9 +73,18 @@ def create_app():
     from api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
+    from routes.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from routes.oauth2 import bp as oauth2_bp
+    app.register_blueprint(oauth2_bp, url_prefix='')
+
     from routes.home import bp as home_bp
     app.register_blueprint(home_bp, url_prefix='')
 
+    app.cli.add_command(create_clients, 'create-clients')
+    # @app.cli.command("create-clients")
+    
     return app
 
 # def setup_app(app):
