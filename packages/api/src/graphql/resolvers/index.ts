0a14377eff10ca5938a91908186a37ed7ef5bc42
macintoshhelper <macintoshhelper@users.noreply.github.com>
2023-04-02 00:03:38 +0400

    add oauth query/resolver GraphQL schema/resolver

--- packages/api/src/graphql/resolvers/index.ts
+++ packages/api/src/graphql/resolvers/index.ts
@@ -4,11 +4,13 @@ import scalarResolver from './scalar.resolver';
 
 import userResolver from './user.resolver';
 import utilResolver from './util.resolver';
+import oAuthResolver from './oauth.resolver';
 
 const resolvers = [
   scalarResolver,
   userResolver,
   utilResolver,
+  oAuthResolver,
 ];
 
 export default mergeResolvers(resolvers);
