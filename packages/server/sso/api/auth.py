6a8d624d9f640b5b17eebee7d80836b07b98d988
macintoshhelper <macintoshhelper@users.noreply.github.com>
2023-04-02 00:13:14 +0400

    create flask login API route with internal authorization code flow

--- packages/server/sso/api/auth.py
+++ packages/server/sso/api/auth.py
@@ -3,19 +3,112 @@ from authlib.integrations.flask_oauth2 import current_token
 from api.schemas import UserSchema, LoginSchema, SignupSchema
 from apifairy import authenticate, body, response
 from authlib.integrations.flask_oauth2 import current_token
+import copy
 
 
 from api import bp as api
-from oauth2 import require_oauth
-from models import User
+from oauth2 import require_oauth, AuthorizationCodeGrant
+from models import User, OAuth2Client
 from api.errors import http_error
 from db import db
 from oauth2 import authorization
+
+import re
+import base64
+
+login_id_pattern = "^[A-Z1-9]{16}$"
+
 
 
 user_schema = UserSchema()
 login_schema = LoginSchema()
 signup_schema = SignupSchema()
+
+# def save_authorization_code(request, user):
+#     # Generate authorization code
+#     code = oauth.generate_authorization_code(request.client, user)
+
+#     # Save authorization code to database
+#     auth_code = OAuth2AuthorizationCode(
+#         client_id=request.client.client_id,
+#         user_id=user.id,
+#         code=code['code'],
+#         redirect_uri=request.redirect_uri,
+#         response_type=request.response_type,
+#         scope=request.scope
+#     )
+#     db.session.add(auth_code)
+#     db.session.commit()
+
+#     return code
+
+@api.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+
+        auth_header = request.headers.get('Authorization')
+        if not auth_header or not auth_header.startswith('Basic '):
+            return jsonify({'error': 'Missing or invalid Authorization header.'}), 401
+
+        try:
+            auth_decoded = base64.b64decode(auth_header[6:]).decode()
+            client_id, client_secret = auth_decoded.split(':', 1)
+        except:
+            return jsonify({'error': 'Malformed Authorization header.'}), 401
+
+        client = OAuth2Client.query.filter_by(client_id=client_id).first()
+        if not client or client.client_secret != client_secret:
+            return jsonify({'error': 'Invalid client credentials.'}), 401
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = None
+        if (re.match(login_id_pattern, username)):
+            user = User.query.filter_by(login_id=username).first()
+        else:
+            user = User.query.filter_by(unverified_email=username).first()
+        if user is not None and user.verify_password(password):
+            oauth2_request = authorization.create_oauth2_request(request)
+
+            oauth2_request.client = client
+
+            oauth2_request.data = {
+                'code_challenge': request.form.get('code_challenge'),
+                'code_challenge_method': request.form.get('code_challenge_method'),
+                'redirect_uri': request.form.get('redirect_uri', '')
+            }
+            oauth2_request.user = user
+
+            grant = AuthorizationCodeGrant(oauth2_request, authorization)
+
+            code = grant.generate_authorization_code()
+            grant.save_authorization_code(code, oauth2_request)
+
+            return jsonify({ 'code': code })
+        else:
+            return 'Invalid credentials';
+        #     return user
+
+        # # Verify username and password
+
+        # user = users.get(username)
+        # if user and user['password'] == password:
+        #     # Generate a random authorization code
+        #     code = generate_token(48)
+
+
+        #     authorization.createa
+        #     # Store the code in a database or cache
+        #     # Redirect to the authorization endpoint with the code and state
+        #     return redirect(authorization.create_authorization_response(
+        #         code=code,
+        #         state=request.args.get('state')
+        #     ))
+        # else:
+        #     # Incorrect username or password
+        #     return 'Invalid credentials'
+    return None
 
 @api.route('/auth/email/confirm/<token>')
 @require_oauth()
