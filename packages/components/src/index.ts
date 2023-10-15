9c2d200f9cd7a571744b8f8e71e29961aadec236
macintoshhelper <macintoshhelper@users.noreply.github.com>
2023-10-15 02:45:30 +0100

    migrate common comps to sso component lib

---
+++ packages/components/src/index.ts
@@ -0,0 +1,18 @@
+import Section from './Section';
+
+import { Link, Text, TextLink, FormTextInput } from './common';
+
+import useWindowViewport from './hooks/use-window-viewport';
+
+export {
+  useWindowViewport,
+};
+
+export {
+  Link,
+  TextLink,
+  Text,
+  Section,
+  FormTextInput,
+};
+
--- packages/web/components/common/index.ts
+++ packages/web/components/common/index.ts
@@ -1,2 +1,1 @@
-export { default as Text, TextLink } from './Text';
-export { default as Link } from './Link';
+export { default as FormTextInput } from './FormTextInput';
